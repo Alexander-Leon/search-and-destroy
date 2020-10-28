7fa1679ba75f9b089c6424a4d29ea2bbfb39471d
Alexander Leon <1995.Alexander.Leon@gmail.com>
2020-10-28 15:09:48 -0700

    jump man solution;

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,21 +2,65 @@
 
 // Complete this algo
 const minJumps = arr => {
-  let currentValue = 0;
+  let reach = arr[0]
+  let current = arr[0]
   let layer = 1;
-  while(true) {
+  for (let i =1; i < arr.length; i++){
+      if (i === (arr.length-1)){
+          return layer
+      }
+      reach = Math.max(reach, i + arr[i])
+      current--;
+      if(current === 0){
+          layer++
+          if (i >= reach){
+              return -1
+          }
+          current = reach - i
+      }
+
+  }
+  
+  
+  /*while(true) {
     let largestValIdx = currentValue + 1;
     for(let i = currentValue + 1; i < arr[currentValue]; i++) {
-      if(arr[i] === arr[arr.length]) return layer;
+      if(arr[i] === arr[arr.length-1]) return layer;
       if(arr[i] > arr[largestValIdx]) largestValIdx = i;
     }
     layer++;
     currentValue = largestValIdx;
     console.log(currentValue, layer);
-  }
+  }*/
 };
 
 module.exports = minJumps
+
+
+/*const minJumps = arr => {
+let reach = arr[0];
+let step = arr[0];
+let jump = 1
+for (let i = 1; i < arr.length; i++){
+    if (i === (arr.length-1)){
+        return jump
+    }
+    console.log(reach, step)
+    reach = Math.max(reach, i + arr[i]);
+    step --;
+    if (step == 0) {
+        jump++
+        step = reach - i
+    }
+}
+};*/
+
+
+
+
+
+
+
 
 
 /*
